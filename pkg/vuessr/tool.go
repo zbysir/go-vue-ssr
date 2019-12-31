acc7a22b12430180e7926cc6d69ecfb7e385f7e8
zhangliang <zhangliang@zhuzi.me>
2019-12-31 16:56:57 +0800

    添加编译缓存

---
+++ pkg/vuessr/tool.go
@@ -0,0 +1,20 @@
+package vuessr
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+func Md5(in []byte) (out []byte) {
+	h := md5.New()
+	h.Write(in)
+	mbs := h.Sum(nil)
+	out = make([]byte, hex.EncodedLen(len(mbs)))
+	hex.Encode(out, mbs)
+
+	return
+}
+func Md5String(in string) (out string) {
+	out = string(Md5([]byte(in)))
+	return
+}
